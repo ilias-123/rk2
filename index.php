bfeed26767426a583cade493e0595ff8161e60c2
ilias-123 <56249257+ilias-123@users.noreply.github.com>
2019-11-11 11:15:43 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse:collapse;
+}
+</style>
+</head>
+<body>
+<h1> Umitbaev Ilias , IU4-13Б , Variant 3 </h1>
+<h2> task1 </h2>
+<?php
+echo "<table border=1>";
+for( $a=1; $a<11; $a++)
+{ 
+echo "<tr>";
+for( $b=1; $b<11; $b++)
+{ echo "<td>" , $a*$b , "</td>";}
+} echo "</tr>";
+echo "</table>";
+?>;
+</body>
+</html>
